fe0946ae0e5d5849b878d45715052a58f3320060
ozumin <mizuo.n@gmail.com>
2024-09-15 14:28:09 +0900

    #291 init

---
+++ CrossPlatformSwift/CrossPlatformSwift/Counter/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Counter",
+    platforms: [.iOS(.v16), .macOS(.v14)],
+    products: [
+        .library(
+            name: "Counter",
+            targets: ["Counter"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-navigation", from: "2.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-perception", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "Counter",
+            dependencies: [
+                .product(name: "SwiftNavigation", package: "swift-navigation"),
+                .product(name: "Perception", package: "swift-perception")
+            ]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
---
+++ CrossPlatformSwift/CrossPlatformSwift/CrossPlatformSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossPlatformSwiftApp.swift
+//  CrossPlatformSwift
+//
+//  Created by Mizuo Nagayama on 2024/09/15.
+//
+
+import SwiftUI
+
+@main
+struct CrossPlatformSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
